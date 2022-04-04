67ff10740aeafc5efcdf6c651752ce1c8d0cc710
Davisralves <davialves324@gmail.com>
2022-04-04 13:41:17 -0300

    adicionando player

--- script.js
+++ script.js
@@ -4,23 +4,22 @@ const c = canvas.getContext('2d');
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+const map = new Image();
+map.src = './img/test-map-rpg.png';
 const player = new Image();
 player.src = './img/player/playerDown.png';
-
-const map = new Image();
-// map.src = './img/empty.png';
-// map.onload = () => {
-// c.drawImage(image, -700, -500);
+map.onload = () => {
+c.drawImage(map, -700, -500);
+  player.onload = () => c.drawImage(
+  player,
+  0,
+  0,
+  player.width / 4,
+  player.height,
+  centerWidth, 
+  centerHeight,
+  player.width / 4,
+  player.height);
 const centerWidth = canvas.width / 2 - (player.width / 4) / 2; 
 const centerHeight = canvas.height / 2 - player.height / 2; 
- player.onload = () => c.drawImage(
-   player,
-   0,
-   0,
-   player.width / 4,
-   player.height,
-   centerWidth, 
-   centerHeight,
-   player.width / 4,
-   player.height)
-// }
+  } 
